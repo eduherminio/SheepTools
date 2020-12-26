9f377fc0c85ed66d00276fc315f5a42d008a4a50
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2020-12-26 22:01:49 +0100

    Add BitArrayComparer

---
+++ src/SheepTools/BitArrayComparer.cs
@@ -0,0 +1,19 @@
+﻿using SheepTools.Extensions;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SheepTools
+{
+    public class BitArrayComparer : IEqualityComparer<BitArray>
+    {
+        public bool Equals(BitArray? x, BitArray? y)
+        {
+            return x?.ToBitString() == y?.ToBitString();
+        }
+
+        public int GetHashCode(BitArray obj)
+        {
+            return obj.ToBitString().GetHashCode();
+        }
+    }
+}
