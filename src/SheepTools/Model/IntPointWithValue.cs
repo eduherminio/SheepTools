342a17afc53927630abe746ddbf0eb4c8822c516
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2022-12-09 22:34:47 +0100

    Add `IntPointWithValue`

---
+++ src/SheepTools/Model/IntPointWithValue.cs
@@ -0,0 +1,22 @@
+﻿namespace SheepTools.Model;
+
+/// <summary>
+/// Lightweight <see cref="Point"/> record class, based on <see cref="int"/> and with value
+/// </summary>
+public record IntPointWithValue<T> : IntPoint
+{
+    public T? Value { get; set; }
+
+    public IntPointWithValue(int x, int y) : base(x, y)
+    {
+    }
+
+    public IntPointWithValue(T value, int x, int y) : base(x, y)
+    {
+        Value = value;
+    }
+
+#pragma warning disable RCS1132 // Remove redundant overriding member - not redundant, https://github.com/JosefPihrt/Roslynator/issues/744
+    public override string ToString() => base.ToString();
+#pragma warning restore RCS1132 // Remove redundant overriding member.
+}
---
+++ tests/SheepTools.Test/Model/IntPointWithValueTest.cs
@@ -0,0 +1,34 @@
+﻿using SheepTools.Model;
+using Xunit;
+
+namespace SheepTools.Test.Model;
+
+public class IntPointWithValueTest
+{
+    [Fact]
+    public void Equal()
+    {
+        var a = new IntPointWithValue<string>(0, 0);
+        var b = new IntPointWithValue<string>(0, 0);
+        var c = new IntPointWithValue<string>(0, 1);
+
+        Assert.Equal(a, b);
+        Assert.NotEqual(a, c);
+        Assert.True(a.Equals(b));
+        Assert.True(a == b);
+        Assert.True(a != c);
+
+        HashSet<IntPointWithValue<string>> set = new() { a };
+        Assert.False(set.Add(b));
+        Assert.True(set.Add(c));
+    }
+
+    [Fact]
+    public void ToStringTest()
+    {
+        var p = new IntPointWithValue<string>("Value", 0, 1);
+
+        Assert.Equal($"[{p.X}, {p.Y}]", p.ToString());
+        Assert.Equal("Value", p.Value);
+    }
+}
